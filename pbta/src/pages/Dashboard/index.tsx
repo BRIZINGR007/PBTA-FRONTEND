13737b02c0bf52f45ae03eefe10af9e2553aa600
Nabarka Deb <nabarka.deb@u-next.com>
2025-05-15 08:21:23 +0530

    Added some changes  for  the  dashboard  ...

--- pbta/src/pages/Dashboard/index.tsx
+++ pbta/src/pages/Dashboard/index.tsx
@@ -2,8 +2,21 @@ import { useState } from 'react';
 import Transactions from './components/Transactions';
 import TransactionSummary from './components/TransactionSummary';
 import './Dashboard.css'
+import useGetTransactions from '../../hooks/useGetTransactions';
+
+
+
 const Dashboard: React.FC = () => {
     const [month, setMonth] = useState('2025-05-01');
+    const {
+        transactions,
+        loading,
+        numPages,
+        currentPage,
+        setCurrentPage,
+        refetchTrans
+    } = useGetTransactions(month, 1);
+
     const handleLogout = () => {
         // Add your logout logic here
         console.log("Logged out");
@@ -17,8 +30,14 @@ const Dashboard: React.FC = () => {
                 </div>
             </nav>
             <div className="dashboard-content">
-                <TransactionSummary month={month} setMonth={setMonth} />
-                <Transactions month={month} />
+                <TransactionSummary month={month} setMonth={setMonth} refetchTrans={refetchTrans} />
+                <Transactions
+                    transactions={transactions}
+                    loading={loading}
+                    numPages={numPages}
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                />
 
             </div>
         </>
