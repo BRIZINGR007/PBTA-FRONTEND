d8624fb64840b557f677f9a0f8862607895468ae
Nabarka Deb <nabarka.deb@u-next.com>
2025-05-17 13:24:18 +0530

    Changed  the constants  ..

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,5 @@
 // src/constants.ts
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://pbta-backend.onrender.com";
+
+
+// export const API_BASE_URL = "http://localhost:8000";
